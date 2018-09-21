223626c6b4d915ca90e4af484fcd050d4e16c406
anilgithub2018 <anil.waghmare@gmail.com>
2018-09-21 15:13:48 -0400

    sep 9 3pm

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const app = express()
 
+console.log("entering the app");
 app.get('/', (req, res) => res.send('Hello from Google App Engine!'))
 
 app.get('/teapot', (req, res) => {
   res.status(418).send("Hello I'm a teapot running on Node Standard GAE")
 })
 
+console.log("started to listen" +  process.env.port );
 app.listen(process.env.port || 8080, () =>
-  console.log('Example app listening on port 3000!')
+  console.log('Example app listening on port 8080!')
 )
