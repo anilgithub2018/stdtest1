6c464664ba6cb5409759a8a2d32edb5d6c423136
Waghmare <waghmaa@amrl2tw07hhtdh.pfizer.com>
2018-09-29 08:27:28 -0400

    Sep 29 826am

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get('/teapot', (req, res) => {
   res.status(418).send("Hello I'm a teapot running on Node Standard GAE")
 })
 
-console.log("started to listen" +  process.env.port );
+console.log("started to listen" +  process.env.PORT );
 app.listen(process.env.port || 8080, () =>
   console.log('Example app listening on port 8080!')
 )
